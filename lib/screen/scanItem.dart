7f20b69043bacd9882f0e83d5d52eded4b3158f1
KorOkami <korokami2533@gmail.com>
2023-04-17 14:15:49 +0700

    Edit Fail Login

--- lib/screen/scanItem.dart
+++ lib/screen/scanItem.dart
@@ -26,130 +26,136 @@ class _Scan_ItemState extends State<Scan_Item> {
   final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Form(
-        key: formKey,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-                "${widget.bu_detail.buCode} - ${widget.bu_detail.whsCode}",
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                  "${widget.bu_detail.buCode} - ${widget.bu_detail.whsCode}",
+                  style: GoogleFonts.prompt(
+                      fontSize: 40, color: Color.fromARGB(255, 1, 68, 122))),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Item QR/Barcode",
                 style: GoogleFonts.prompt(
-                    fontSize: 40, color: Color.fromARGB(255, 1, 68, 122))),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text("Item QR/Barcode",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83))),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                  child: TextFormField(
-                controller: _textEditingController,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Scan Item',
-                ),
-              )),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 8, 18),
-                child: IconButton(
-                  alignment: Alignment.topCenter,
-                  onPressed: startScan,
-                  icon: Icon(
-                    Icons.qr_code_scanner,
-                    color: Colors.black,
-                    size: 50,
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83))),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: TextFormField(
+                  controller: _textEditingController,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Scan Item',
                   ),
+                  onChanged: (value) {
+                    print("Hey");
+                  },
+                )),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 8, 18),
+                  child: IconButton(
+                    alignment: Alignment.topCenter,
+                    onPressed: startScan,
+                    icon: Icon(
+                      Icons.qr_code_scanner,
+                      color: Colors.black,
+                      size: 50,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              child: Text(
+                "Item Name :",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              child: Text(
+                "Location :",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              child: Text(
+                "Base Uom :",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              child: Text(
+                "Batch",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              child: Text(
+                "Onhand :",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              child: Text(
+                "Counted :",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              child: Text(
+                "Count :",
+                style: GoogleFonts.prompt(
+                    fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton.icon(
+                label: Text(
+                  "ยืนยัน",
+                  style: GoogleFonts.prompt(fontSize: 20, color: Colors.white),
                 ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            child: Text(
-              "Item Name :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            child: Text(
-              "Location :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            child: Text(
-              "Base Uom :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            child: Text(
-              "Batch",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            child: Text(
-              "Onhand :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            child: Text(
-              "Counted :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            child: Text(
-              "Count :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            width: double.infinity,
-            height: 50,
-            child: ElevatedButton.icon(
-              label: Text(
-                "ยืนยัน",
-                style: GoogleFonts.prompt(fontSize: 20, color: Colors.white),
-              ),
-              icon: Icon(
-                Icons.check_rounded,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                if (formKey.currentState?.validate() == true) {
-                  formKey.currentState?.save();
+                icon: Icon(
+                  Icons.check_rounded,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  if (formKey.currentState?.validate() == true) {
+                    formKey.currentState?.save();
 
-                  //formKey.currentState?.reset();
-                }
-              },
-            ),
-          )
-        ]),
+                    //formKey.currentState?.reset();
+                  }
+                },
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
