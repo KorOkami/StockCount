a68cf2d06381b0cace541930280577ea01bcc0f8
KorOkami <korokami2533@gmail.com>
2023-04-21 14:36:07 +0700

    add consumer

--- lib/screen/counting_view.dart
+++ lib/screen/counting_view.dart
@@ -5,6 +5,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:stock_counting_app/model/bu_detail.dart';
 import 'package:stock_counting_app/model/itemMaster.dart';
 
+import 'package:provider/provider.dart';
+import 'package:stock_counting_app/model/stockOnhand.dart';
+import 'package:stock_counting_app/providers/batch_provider.dart';
+
 class Counting_View extends StatefulWidget {
   const Counting_View({super.key});
   //final ItemMaster itm_detail;
@@ -16,34 +20,60 @@ class Counting_View extends StatefulWidget {
 class _Counting_ViewState extends State<Counting_View> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            child: Text(
-              "Item Code :",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) {
+          return Batch_Provider();
+        })
+      ],
+      child:
+          Consumer(builder: (context, Batch_Provider provider, Widget? child) {
+        return Column(
+          children: [
+            SizedBox(
+              child: Text(
+                "Item Code : ${provider.bList[0].itemCode}",
+                style: GoogleFonts.prompt(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 1, 57, 83)),
+              ),
             ),
-          ),
-          SizedBox(
-            child: Text(
-              "Item Name : ",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+            SizedBox(
+              child: Text(
+                "Item Name : ${provider.bList[0].itemName}",
+                style: GoogleFonts.prompt(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 1, 57, 83)),
+              ),
             ),
-          ),
-          SizedBox(
-            child: Text(
-              "Base Uom : ",
-              style: GoogleFonts.prompt(
-                  fontSize: 20, color: Color.fromARGB(255, 1, 57, 83)),
+            SizedBox(
+              child: Text(
+                "Base Uom : ${provider.bList[0].uomName}",
+                style: GoogleFonts.prompt(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 1, 57, 83)),
+              ),
             ),
-          ),
-        ],
-      ),
+            ListView.builder(
+                itemCount: provider.bList.length,
+                itemBuilder: (context, int index) {
+                  StockOnhand data = provider.bList[index];
+                  return Card(
+                    elevation: 5,
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: ListTile(
+                      title: Text("Batch : ${data.batchId}"),
+                      subtitle: Text("OnHand : ${data.qty}"),
+                    ),
+                  );
+                })
+          ],
+        );
+      }),
     );
   }
 }
