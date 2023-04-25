c665c983bf3e34f6747cebdcd798b33bbf4d7e9e
KorOkami <korokami2533@gmail.com>
2023-04-25 14:39:19 +0700

    add Counting Detail

---
+++ lib/model/countingDetail.dart
@@ -0,0 +1,48 @@
+// To parse this JSON data, do
+//
+//     final countingDetail = countingDetailFromJson(jsonString);
+
+import 'dart:convert';
+
+List<CountingDetail> countingDetailFromJson(String str) =>
+    List<CountingDetail>.from(
+        json.decode(str).map((x) => CountingDetail.fromJson(x)));
+
+String countingDetailToJson(List<CountingDetail> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class CountingDetail {
+  CountingDetail({
+    this.id,
+    this.itemCode,
+    this.batchId,
+    this.expiryDate,
+    this.countQty,
+    this.userName,
+  });
+
+  String? id;
+  String? itemCode;
+  String? batchId;
+  String? expiryDate;
+  int? countQty;
+  String? userName;
+
+  factory CountingDetail.fromJson(Map<String, dynamic> json) => CountingDetail(
+        id: json["id"],
+        itemCode: json["itemCode"],
+        batchId: json["batchID"],
+        expiryDate: json["expiryDate"],
+        countQty: json["countQty"],
+        userName: json["userName"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "itemCode": itemCode,
+        "batchID": batchId,
+        "expiryDate": expiryDate,
+        "countQty": countQty,
+        "userName": userName,
+      };
+}
