cbbdabbaddbb2d0d9f1dcac5860c954243a3401b
KorOkami <korokami2533@gmail.com>
2023-05-31 09:40:33 +0700

    Set Time Interval

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,10 +3,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dio/dio.dart';
 import 'package:stock_counting_app/interceptors/dio_interceptor.dart';
 import 'package:stock_counting_app/model/bu_detail.dart';
+import 'package:stock_counting_app/model/countingDoc.dart';
 import 'package:stock_counting_app/model/profile.dart';
 import 'package:stock_counting_app/model/stockOnhand.dart';
 import 'package:stock_counting_app/model/successlogin.dart';
 import 'package:stock_counting_app/services/store.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as json;
 
 class stockCountingAPI {
   late final Dio _dio;
@@ -31,31 +35,70 @@ class stockCountingAPI {
       "email": "$user",
       "password": "$password"
     };
-    final response = await _dio.post(_loginUrl, data: _logindata);
-    if (response.statusCode == 200) {
-      await _saveToken(response.data);
-      //successloginFromJson(response.data);
-      res.status = "success";
-      res.token = response.data['token'];
-      res.username = response.data['userName'];
-      return res;
-    } else {
+    try {
+      final response = await _dio.post(_loginUrl, data: _logindata);
+      if (response.statusCode == 200) {
+        await _saveToken(response.data);
+        //successloginFromJson(response.data);
+        res.status = "success";
+        res.token = response.data['token'];
+        res.username = response.data['userName'];
+        Timer mytimer = Timer.periodic(Duration(minutes: 1), (timer) {
+          refershtoken();
+          //mytimer.cancel() //to terminate this timer
+        });
+        return res;
+      }
+    } on DioError catch (e) {
+      res.status = "fail";
+      res.ErrorM = e.response?.data;
       return res;
     }
   }
 
+  Future<void> refershtoken() async {
+    /*var headers = {
+      'Content-Type': 'application/json',
+      'Cookie': 'refreshToken=l9lbOu2%2BQ08KTAKEUHqYe9fwywz6Rz5TKeXP7yQV2p0%3D'
+    };*/
+
+    final response = await _dio
+        .post('https://inventory-uat.princhealth.com/api/account/refreshToken');
+    if (response.statusCode == 200) {
+      await _saveToken(response.data);
+    } else {}
+  }
+
   Future<List<StockOnhand>> GetBatchList(
       String bu_detail_Id, String itemCode) async {
-    late List<StockOnhand> _StockOnhand = List.empty();
+    late List<StockOnhand> _StockOnhand = [];
     String _BatchListUrl =
         'https://inventory-uat.princhealth.com/api/stockcounts/${bu_detail_Id}/item?ItemCode=${itemCode}';
     final response = await _dio.get(_BatchListUrl);
 
     if (response.statusCode == 200) {
+      response.data.forEach((e) {
+        _StockOnhand.add(StockOnhand.fromJson(e));
+      });
       //_StockOnhand = stockOnhandFromJson(Response.body);
     } else {
       String test = "";
     }
     return _StockOnhand;
+  }
+
+  Future<List<CountingDoc>> GetBU() async {
+    late List<CountingDoc> _DocumentCounting = [];
+
+    String _BuListUrl =
+        'https://inventory-uat.princhealth.com/api/stockcounts/mobile';
+    final response = await _dio.get(_BuListUrl);
+    if (response.statusCode == 200) {
+      //_DocumentCounting = countingDocFromJson(response.data);
+      response.data.forEach((e) {
+        _DocumentCounting.add(CountingDoc.fromJson(e));
+      });
+    } else {}
+    return _DocumentCounting;
   }
 }
